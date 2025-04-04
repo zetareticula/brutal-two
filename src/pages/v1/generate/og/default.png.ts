d8aa4e39ffe53f7f9df7c29767bc409624f4c958
Zeta Reticula <zetareticulae@proton.me>
2025-04-04 16:50:19 -0700

    embedded prototype
    This version contains the chatbot.

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -19,9 +19,9 @@ export const GET: APIRoute = async () => {
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">Brutal</p>
-          <p style="font-size: 38px;">A theme for Astro</p>
-          <p style="font-size: 38px;">Brought to you by Elian</p>
+          <p style="font-size: 48px;">Klever</p>
+          <p style="font-size: 38px;">Aging</p>
+          <p style="font-size: 38px;">Brought to you by Klever Inc</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
